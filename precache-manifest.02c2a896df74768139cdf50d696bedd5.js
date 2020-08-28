50a1e1cd64cad3df39593c0e23f169362feddf0c
Smesl <maks.mazu@mail.ru>
2020-08-28 17:38:49 +0500

    Updates

---
+++ precache-manifest.02c2a896df74768139cdf50d696bedd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec6eea64554acbb07049c8dc7651ed3",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "ff64b88e3fa21cd530b4",
+    "url": "/react/static/css/main.e156ef55.chunk.css"
+  },
+  {
+    "revision": "5ee85e948fa946616729",
+    "url": "/react/static/js/2.41e142e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react/static/js/2.41e142e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff64b88e3fa21cd530b4",
+    "url": "/react/static/js/main.bdbc9da0.chunk.js"
+  },
+  {
+    "revision": "70ce701a60455ccf9ac8",
+    "url": "/react/static/js/runtime-main.ae583e3c.js"
+  }
+]);
